36ab058db51be0101f5d4d6df2beb44c94bc1780
Richard van den Broek <vandenbroek.rp@gmail.com>
2021-12-29 23:24:58 +0100

    add spatial layout to section

--- api_seating/models/section.py
+++ api_seating/models/section.py
@@ -1,7 +1,15 @@
 from django.db import models
+POSITION_CHOICES = [
+  ('l', 'left'),
+  ('c', 'centre'),
+  ('r', 'right'),
+  ('ll', 'outer left'),
+  ('rr', 'outer right')
+]
 
 class Section(models.Model):
     name = models.CharField(max_length=50)
+    position = models.CharField(max_length=50, choices=POSITION_CHOICES, default='c')
     is_balcony = models.BooleanField(default=False)
     is_loge = models.BooleanField(default=False)
 
