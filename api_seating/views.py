e31947e0279fdad34a0f4c694401f3fca748b7b1
Richard van den Broek <vandenbroek.rp@gmail.com>
2021-12-30 13:10:41 +0100

    created order.json for api post testing

--- api_seating/views.py
+++ api_seating/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from .serializers import OrderSerializer
 from .models import Order
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
